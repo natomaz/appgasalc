0e236648562706de0349102efef91e9084133d7b
natomaz <65260903+natomaz@users.noreply.github.com>
2021-10-17 18:24:27 -0300

    app

--- app/src/main/java/com/example/gasolinaoualcool/MainActivity.java
+++ app/src/main/java/com/example/gasolinaoualcool/MainActivity.java
@@ -3,15 +3,20 @@ package com.example.gasolinaoualcool;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
     private EditText edtAlcool, edtGasolina;
     private Button btnCompara;
     private TextView txtResult1, txtResult2;
-    
+    public double valorAl, valorGas, percentual, resultado;
+    public  String textoResult = "";
+    DecimalFormat f = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
         iniciarComponentes();
+        btnCompara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valorAl = Double.parseDouble(edtAlcool.getText().toString());
+                valorGas = Double.parseDouble(edtGasolina.getText().toString());
+                percentual = valorAl/valorGas * 100;
+                txtResult1.setText(f.format(percentual)+"%");
+                if(percentual > 73){
+                    textoResult = "Opte por gasolina";
+                }else{
+                    textoResult = "Opte por álcool";
+                }
+                txtResult2.setText(textoResult);
+            }
+        });
     }
 
     private void iniciarComponentes() {
